70b22b3be14c706f54a35f564e6ed2951ba13076
MancaStrah <manca.strah@gmail.com>
2022-05-10 15:45:13 +0200

    test

---
+++ instrukcije.py
@@ -0,0 +1,50 @@
+# kopirano iz datoteke z vaj
+
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+# uvozimo bottle.py
+from bottleext import *
+
+# uvozimo ustrezne podatke za povezavo
+import auth_public as auth
+
+# uvozimo psycopg2
+import psycopg2, psycopg2.extensions, psycopg2.extras
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) 
+# se znebimo problemov s šumniki
+
+import os
+
+# privzete nastavitve
+SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
+RELOADER = os.environ.get('BOTTLE_RELOADER', True)
+DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
+
+# priklopimo se na bazo
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+#conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+######################################################################
+# ostalo
+
+@get('/')
+def index():
+    return 'Začenta stran, test. Dodava možnost prijave in registracije.'
+
+
+    
+
+
+
+
+
+######################################################################
+# Glavni program
+
+
+# poženemo strežnik na podanih vratih, npr. http://localhost:8080/
+if __name__ == "__main__":
+    run(host='localhost', port=SERVER_PORT, reloader=RELOADER)
+
