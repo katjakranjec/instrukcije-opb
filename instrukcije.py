d8f476d4466e8d7e9cf363eb104f11cbd5f70b46
katjakranjec <kranjec.katja1@gmail.com>
2022-05-15 18:48:19 +0200

    poskus prijave

--- instrukcije.py
+++ instrukcije.py
@@ -15,34 +15,81 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 # se znebimo problemov s šumniki
 
 import os
+import hashlib
+
+skrivnost="skrivnost"
 
 # privzete nastavitve
 SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
 RELOADER = os.environ.get('BOTTLE_RELOADER', True)
 DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
 
-# priklopimo se na bazo
-conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
-#conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+#debug(True)
 
 ######################################################################
 # ostalo
+
+def nastaviSporocilo(sporocilo = None):
+    # global napakaSporocilo
+    staro = request.get_cookie("sporocilo", secret=skrivnost)
+    if sporocilo is None:
+        response.delete_cookie('sporocilo') #če funkciji ne podamo ničesar, izbriše piškotek z imenom sporočilo
+    else:
+        response.set_cookie('sporocilo', sporocilo, path="/", secret=skrivnost)
+    return staro
+
+
+
+#_____________________________________________________________
 
 @get('/')
 def index():
     return 'Začenta stran, test. Dodava možnost prijave in registracije.'
 
+#_PRIJAVA______________________________________________
 
-    
+@get('/prijava') 
+def prijava_get():
+    return template('prijava.html', napaka=None)
 
+@post('/prijava')
+def prijava_post():
+    username = request.forms.username
+    geslo = request.forms.password
+    if username is None or geslo is None:
+        nastaviSporocilo('Uporabniško ime in geslo morata biti neprazna') 
+        redirect(url('prijava_get'))
+    cur = baza.cursor()
+    hgeslo = None
+    try: 
+        cur.execute("SELECT geslo FROM oseba WHERE uporabnisko_ime = %s", (username, ))
+        hgeslo, = cur.fetchone()
+    except:
+        hgeslo = None
+    if hgeslo is None:
+        nastaviSporocilo('Uporabniško ime ali geslo nista pravilna')
+        redirect(url('prijava_get'))
+        return
+    if geslo != hgeslo:
+        nastaviSporocilo('Uporabniško ime ali geslo nista pravilna') 
+        redirect(url('prijava_get'))
+        return
+    response.set_cookie('username', username, path="/", secret=skrivnost)
+    redirect(url('uporabnik'))
 
+@get('/uporabnik') 
+def uporabnik():
+    return 'Dela.'
 
 
 
 ######################################################################
 # Glavni program
 
+# priklopimo se na bazo
+baza = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+#conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+cur = baza.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 # poženemo strežnik na podanih vratih, npr. http://localhost:8080/
 if __name__ == "__main__":
