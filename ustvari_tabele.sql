308e66d56fdcc58cd5ef9103a738ef2c9e840a29
katjakranjec <kranjec.katja1@gmail.com>
2022-05-15 17:43:30 +0200

    Prva verzija ustvarjanja tabel

---
+++ ustvari_tabele.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS oseba;
+DROP TABLE IF EXISTS predmet;
+DROP TABLE IF EXISTS solanje;
+DROP TABLE IF EXISTS termin;
+
+CREATE TABLE oseba (
+    id SERIAL PRIMARY KEY,
+    ime TEXT NOT NULL,
+    priimek TEXT NOT NULL,
+    rojstvo DATE NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    telefon TEXT NOT NULL,
+    uporabnisko_ime TEXT NOT NULL UNIQUE,
+    geslo TEXT NOT NULL
+);
+
+CREATE TABLE predmet (
+    id_osebe INTEGER REFERENCES oseba(id),
+    predmet TEXT NOT NULL
+);
+
+CREATE TABLE solanje (
+    id_osebe REFERENCES oseba(id),
+    letnik TEXT NOT NULL,
+    sola TEXT NOT NULL
+);
+
+CREATE TABLE termin (
+    instruktor INTEGER REFERENCES oseba(id),
+    stranka INTEGER REFERENCES oseba(id),
+    predmet TEXT,
+    lokacija TEXT NOT NULL,
+    ocena INTEGER,
+    datum_in_ura DATE NOT NULL
+);
+
+GRANT ALL ON DATABASE sem2022_katjak TO mancast WITH GRANT OPTION;
+GRANT ALL ON SCHEMA public TO mancast WITH GRANT OPTION;
+GRANT CONNECT ON DATABASE sem2022_mancast TO javnost;
+GRANT USAGE ON SCHEMA public TO javnost;
