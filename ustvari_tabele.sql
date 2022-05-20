c32a3a22e0175e20169bbdadcce843ed5b195bd5
katjakranjec <kranjec.katja1@gmail.com>
2022-05-20 20:34:51 +0200

    dodelan sql in napisane vse tabele

--- ustvari_tabele.sql
+++ ustvari_tabele.sql
@@ -3,24 +3,21 @@ DROP TABLE IF EXISTS predmet;
 DROP TABLE IF EXISTS solanje;
 DROP TABLE IF EXISTS termin;
 
-CREATE TABLE oseba (
-    id SERIAL PRIMARY KEY,
-    ime TEXT NOT NULL,
-    priimek TEXT NOT NULL,
-    rojstvo DATE NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    telefon TEXT NOT NULL,
-    uporabnisko_ime TEXT NOT NULL UNIQUE,
-    geslo TEXT NOT NULL,
-    vloga TEXT NOT NULL
-);
-
-
-
--- CREATE TABLE vloga (
---     id_vloge INTEGER PRIMARY KEY,
+-- CREATE TABLE oseba (
+--     id SERIAL PRIMARY KEY,
+--     ime TEXT NOT NULL,
+--     priimek TEXT NOT NULL,
+--     rojstvo DATE NOT NULL,
+--     email TEXT NOT NULL UNIQUE,
+--     telefon TEXT NOT NULL,
+--     uporabnisko_ime TEXT NOT NULL UNIQUE,
+--     geslo TEXT NOT NULL,
 --     vloga TEXT NOT NULL
--- )
+-- );
+
+
+
+
 
 
 
@@ -40,40 +37,74 @@ CREATE TABLE oseba (
     telefon TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     uporabnisko_ime TEXT NOT NULL PRIMARY KEY,
-    geslo TEXT NOT NULL,
-    vloga TEXT NOT NULL
+    geslo TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS vloga;
+CREATE TABLE vloga (
+    ime_vloge TEXT PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS vloga_osebe;
+CREATE TABLE vloga_osebe (
+    oseba TEXT REFERENCES oseba(uporabnisko_ime),
+    vloga TEXT REFERENCES vloga(ime_vloge),
+    PRIMARY KEY (oseba,vloga)
+);
+
+DROP TABLE IF EXISTS predmet;
+CREATE TABLE predmet (
+    ime_predmeta TEXT PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS podrocje;
+CREATE TABLE podrocje (
+    oseba TEXT REFERENCES oseba(uporabnisko_ime),
+    predmet TEXT REFERENCES predmet(ime_predmeta),
+    PRIMARY KEY (oseba,predmet)
+);
+
+DROP TABLE IF EXISTS letnik;
+CREATE TABLE letnik (
+    ime_letnika TEXT PRIMARY KEY
+);
+
+DROP TABLE IF EXISTS obiskuje;
+CREATE TABLE obiskuje (
+    oseba TEXT REFERENCES oseba(uporabnisko_ime),
+    letnik TEXT REFERENCES letnik(ime_letnika),
+    PRIMARY KEY (oseba,letnik)
+);
+
+DROP TABLE IF EXISTS termin;
+CREATE TABLE termin (
+    instruktor TEXT REFERENCES oseba(uporabnisko_ime),
+    stranka TEXT REFERENCES oseba(uporabnisko_ime),
+    predmet TEXT REFERENCES predmet(ime_predmeta),
+    lokacija TEXT NOT NULL,
+    datum_in_ura DATETIME NOT NULL,
+    PRIMARY KEY (instruktor,datum_in_ura)
 );
 
 --primer osebe z vlogo (na roko vnešen v bazo)
-INSERT INTO oseba VALUES ('Katja','Kranjec','123456789','katja@gmail.com','katjak','geslo','stranka');
-INSERT INTO oseba VALUES ('Manca','Strah','987654321','manca@gmail.com','mancast','geslo','instruktor');
+INSERT INTO oseba VALUES ('Katja','Kranjec','123456789','katja@gmail.com','katjak','geslo');
+INSERT INTO oseba VALUES ('Manca','Strah','987654321','manca@gmail.com','mancast','geslo');
 
-CREATE TABLE predmet (
-    id_osebe INTEGER REFERENCES oseba(id),
-    predmet TEXT NOT NULL
-);
+-- CREATE TABLE termin (
+--     instruktor INTEGER REFERENCES oseba(id),
+--     stranka INTEGER REFERENCES oseba(id),
+--     predmet TEXT, 
+--     -- a bi imeli predmeti svojo tabelo in svoje id-je (ne to, ki je že, kdo uči
+--     -- kateri predmet, ampak kao 1 - SLO, 2, MAT ... Al bi blo odveč? Da se ne bo 
+--     -- dogajalo npr. SLO, Slovenščina ... )
 
-CREATE TABLE solanje (
-    id_osebe REFERENCES oseba(id),
-    letnik TEXT NOT NULL,
-    sola TEXT NOT NULL
-);
-
-CREATE TABLE termin (
-    instruktor INTEGER REFERENCES oseba(id),
-    stranka INTEGER REFERENCES oseba(id),
-    predmet TEXT, 
-    -- a bi imeli predmeti svojo tabelo in svoje id-je (ne to, ki je že, kdo uči
-    -- kateri predmet, ampak kao 1 - SLO, 2, MAT ... Al bi blo odveč? Da se ne bo 
-    -- dogajalo npr. SLO, Slovenščina ... )
-
-    -- če že bi lahko bla tabela s kraticami pa predmeti pa je pol kratica primary key.
-    -- id po številkah je pa čist odveč
-    -- ampak pomoje je tud to čist odveč string 'slovenscina' čist dobr dela
-    lokacija TEXT NOT NULL,
-    ocena INTEGER,
-    datum_in_ura DATE NOT NULL
-);
+--     -- če že bi lahko bla tabela s kraticami pa predmeti pa je pol kratica primary key.
+--     -- id po številkah je pa čist odveč
+--     -- ampak pomoje je tud to čist odveč string 'slovenscina' čist dobr dela
+--     lokacija TEXT NOT NULL,
+--     ocena INTEGER,
+--     datum_in_ura DATE NOT NULL
+-- );
 
 GRANT ALL ON DATABASE sem2022_katjak TO mancast WITH GRANT OPTION;
 GRANT ALL ON SCHEMA public TO mancast WITH GRANT OPTION;
@@ -87,4 +118,5 @@ GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO mancast WITH GRANT OPTION;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO javnost;
 
 GRANT INSERT ON oseba TO javnost;
+GRANT INSERT ON vloga TO javnost;
 GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO javnost;
